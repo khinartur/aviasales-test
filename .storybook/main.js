44d2ce7da9833cfe1b1523efda67b1665f1c6683
Arthur Khineltsev <khineltsev@gmail.com>
2021-07-02 00:28:55 +0300

    .storybook config files + ignores

---
+++ .storybook/main.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "core": {
+    "builder": "webpack5"
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      '@': path.resolve('src'),
+    };
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+          },
+        },
+        'sass-loader'
+      ],
+    });
+
+    return config;
+  },
+}
